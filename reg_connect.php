ebc832c48fdbae115f435248cf2d5711101e49bf
ranjaniranj <118736385+ranjaniranj@users.noreply.github.com>
2022-12-13 20:33:34 +0530

    Delete reg_connect.php

--- reg_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $email=$_POST["e"];
-  $pwd=$_POST["pwd"];
-  $con=mysql_connect("localhost","root","");
-  mysql_select_db("Webdev");
-  mysql_query("insert into register (e,pwd) values ('$email','$pwd'");
-  include ("Register.php");
-  mysql_close($con);
-?>
